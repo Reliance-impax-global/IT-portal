6904e9a65458352bc59fd932dfe90dba71ec9e4b
sinAbhishek <sinabhshek979@gmail.com>
2024-03-04 12:06:41 +0530

    prompt update

--- backend/controller/chatgpt.js
+++ backend/controller/chatgpt.js
@@ -20,10 +20,15 @@ export const handlerequest = async (req, res) => {
   console.log(req.body);
 
   try {
-    const prompt = `client wants to develop a ${req.body.projectname} with ${req.body.apps} please generate a technical doumentation for developers to understand.The documentation should include following points :- 1. Introduction 2. System Architecture 3. System requirements 4. User Authentication. 5. User Interface 6. Testing 7. Deployment 8. Maintenance and support .Explain each of the points with minimum 10 bullet points .Give your response in html format.`;
+    // const prompt = `client wants to develop a ${req.body.projectname} with ${req.body.apps} please generate a technical doumentation for developers to understand.The documentation should include following points :- 1. Introduction 2. System Architecture 3. System requirements 4. User Authentication. 5. User Interface 6. Testing 7. Deployment 8. Maintenance and support .Explain each of the points with minimum 10 bullet points .Give your response in html format.`;
+    const prompt =
+      "client want to develop a food ordering app with driver app restro app admin app and customer app please generate a technnical documentation to explain the project to developers,Include suggested tech stack and sdk needed. Phases wise target ,300 words please.";
     console.log(prompt);
     const msg = await main(prompt);
     console.log(msg);
+    const filter = `${msg} convert this into html format`;
+    const final = await main(filter);
+    console.log(final);
     res.status(200).json(msg);
   } catch (err) {
     console.log(err);
